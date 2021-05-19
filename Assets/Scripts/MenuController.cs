e640517d07b2ec04e33c7cb9a1fc874ac41921c0
Daniel Atkinson <hourd.tasa@gmail.com>
2021-05-19 21:51:44 +0100

    Add title screen and controls

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,8 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public Button backButton;
+    public Button playButton;
+    public Button controlsButton;
+    public Button controlsBox;
+    public Button descriptionBox;
+
+    private void Start()
+    {
+        backButton.gameObject.SetActive(false);
+        controlsBox.gameObject.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Spaaaaace");
+    }
+
+    public void Controls()
+    {
+        descriptionBox.gameObject.SetActive(false);
+        playButton.gameObject.SetActive(false);
+        controlsButton.gameObject.SetActive(false);
+        backButton.gameObject.SetActive(true);
+        controlsBox.gameObject.SetActive(true);
+    }
+
+    public void Back()
+    {
+        descriptionBox.gameObject.SetActive(true);
+        playButton.gameObject.SetActive(true);
+        controlsButton.gameObject.SetActive(true);
+        backButton.gameObject.SetActive(false);
+        controlsBox.gameObject.SetActive(false);
     }
 }
